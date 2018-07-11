22d9f782668a9acd2a5ff0f249985a2dbe5db401
William Hendrick <william.hendrick@gmail.com>
2018-07-10 22:04:24 -0500

    first commit

---
+++ camera.py
@@ -0,0 +1,18 @@
+from picamera import PiCamera, Color
+from time import sleep
+
+camera = PiCamera()
+
+camera.start_preview()
+
+for i in range(5):
+    sleep(5)
+    camera.annotate_text = 'William Hendrick'
+    camera.annotate_foreground = Color('yellow')
+    camera.capture('/home/pi/Desktop/rbpi_camera_start/image%s.png' % i)
+
+camera.start_recording('/home/pi/Desktop/rbpi_camera_start/video.h264')
+sleep(10)
+camera.stop_recording()
+
+camera.stop_preview()
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Raspberry Pi - Camera Demo
+This project contains various examples for the camera module for the Raspberry
+Pi devices. The examples assume the use of the official RBP camera modules.
