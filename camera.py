bafada4ffa71200e9607ebed10442b7c10e2d6ae
William Hendrick <william.hendrick@gmail.com>
2018-07-10 22:29:24 -0500

    replaced name with hello world

--- camera.py
+++ camera.py
@@ -5,13 +5,14 @@ camera = PiCamera()
 
 camera.start_preview()
 
+
 for i in range(5):
     sleep(5)
-    camera.annotate_text = 'William Hendrick'
+    camera.annotate_text = 'Hello World'
     camera.annotate_foreground = Color('yellow')
     camera.capture('image%s.png' % i)
 
-camera.start_recording('/home/pi/Desktop/rbpi_camera_start/video.h264')
+camera.start_recording('video.h264')
 sleep(10)
 camera.stop_recording()
 
