e68037f27ef65a28becf8c4fa9a02053e9215329
William Hendrick <william.hendrick@gmail.com>
2018-07-10 22:18:00 -0500

    file path commit

--- camera.py
+++ camera.py
@@ -9,7 +9,7 @@ for i in range(5):
     sleep(5)
     camera.annotate_text = 'William Hendrick'
     camera.annotate_foreground = Color('yellow')
-    camera.capture('/home/pi/Desktop/rbpi_camera_start/image%s.png' % i)
+    camera.capture('image%s.png' % i)
 
 camera.start_recording('/home/pi/Desktop/rbpi_camera_start/video.h264')
 sleep(10)
